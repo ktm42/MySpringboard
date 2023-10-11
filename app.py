96f0d98364e27963a60db1d4f55f753f1f28f609
ktm42 <120536046+ktm42@users.noreply.github.com>
2023-10-10 21:54:29 -0500

    Add files via upload
    "Flask Greet Exercise"

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome!"
+
+@app.route('/welcom/home')
+def welcome_home():
+    return "Welcome home!"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome back!"
+
+
+
